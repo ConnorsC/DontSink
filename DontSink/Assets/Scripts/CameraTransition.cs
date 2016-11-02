29680e961c9c17f18af04d3e5e8e2c55c1695c91
ConnorsC <connorschrisc@gmail.com>
2016-11-02 11:43:50 -0300

    Added a camera transition

---
+++ DontSink/Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTransition : MonoBehaviour {
+
+    public Vector3 pos1;
+    public Vector3 pos2;
+    public float speed = 0.0000002f;
+    public GameObject objectToMove;
+    public int gameState;
+
+    public void OnMouseDown()
+    {
+        objectToMove.transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
