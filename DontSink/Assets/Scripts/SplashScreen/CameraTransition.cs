547df306021d8fef578d8403e723d6e8d7f2abad
CSwinamer <carson.swinamer@gmail.com>
2016-11-15 14:54:22 -0400

    Camera Changes Animator
    Animator, animations, new folder

--- DontSink/Assets/Scripts/SplashScreen/CameraTransition.cs
+++ DontSink/Assets/Scripts/SplashScreen/CameraTransition.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class CameraTransition : MonoBehaviour {
 
-    public Vector3 pos1;
-    public Vector3 pos2;
-    public float speed = 0.0000002f;
-    public GameObject objectToMove;
-    public int gameState;
+    Animator anim;
 
     public void OnMouseDown()
     {
-        objectToMove.transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+        anim = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+        int campan = Animator.StringToHash("MenuSelect");
+        anim.SetTrigger(campan);
     }
 }
