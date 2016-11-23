6a2e8f9d144835c328dc56731d5411b14599e61f
Kevan Lynch <kevanlynch@gmail.com>
2016-11-23 18:12:59 -0400

    Adapted IslandClickScript to not allow the player to click far away islands

--- DontSink/Assets/Scripts/MapScreen/IslandClickScript.cs
+++ DontSink/Assets/Scripts/MapScreen/IslandClickScript.cs
@@ -2,20 +2,82 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class IslandClickScript : GameDriver {
+public class IslandClickScript : MonoBehaviour {
 
     public int island;
+    private GameManagerScript manager;
 
     // Use this for initialization
     void Start()
     {
-
+        manager = manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>();
     }
 
     public void OnMouseDown()
     {
-        manager.SetIsland(island);
-        //print("Island: " + island);
+        switch (island)
+        {
+            case 1:
+                if(manager.GetIsland() == 1 || manager.GetIsland() == 2 || manager.GetIsland() == 3)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 2:
+                if (manager.GetIsland() == 1 || manager.GetIsland() == 2 || manager.GetIsland() == 4)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 3:
+                if (manager.GetIsland() == 1 || manager.GetIsland() == 3 || manager.GetIsland() == 4 || manager.GetIsland() == 7)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 4:
+                if (manager.GetIsland() == 2 || manager.GetIsland() == 3 || manager.GetIsland() == 4 || manager.GetIsland() == 6 || manager.GetIsland() == 7)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 5:
+                if (manager.GetIsland() == 5 || manager.GetIsland() == 6 || manager.GetIsland() == 7 || manager.GetIsland() == 8)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 6:
+                if (manager.GetIsland() == 4 || manager.GetIsland() == 5 || manager.GetIsland() == 6 || manager.GetIsland() == 7 )
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 7:
+                if (manager.GetIsland() == 3 || manager.GetIsland() == 4 || manager.GetIsland() == 5 || manager.GetIsland() == 6 || manager.GetIsland() == 7)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+            case 8:
+                if (manager.GetIsland() == 7 || manager.GetIsland() == 8)
+                {
+                    manager.SetIsland(island);
+                    LoadLevel();
+                }
+                break;
+        }
+    }
+    private void LoadLevel()
+    {
+        // Will have more options soon
         manager.LoadLevel("CombatScreen");
     }
     
