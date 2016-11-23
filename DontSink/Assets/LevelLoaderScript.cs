4d6baf27f2d58b02fc5b848a0ee20064112dd318
CSwinamer <carson.swinamer@gmail.com>
2016-11-23 14:28:22 -0400

    Basic fix for portscene
    allows for level changes

---
+++ DontSink/Assets/LevelLoaderScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoaderScript : MonoBehaviour {
+
+    public void onClick()
+    {
+        print("load level!");
+        LoadDialogueScene();
+    }
+
+    public void LoadDialogueScene()
+    {
+        SceneManager.LoadScene("DialogueScreen");
+    }
+}
