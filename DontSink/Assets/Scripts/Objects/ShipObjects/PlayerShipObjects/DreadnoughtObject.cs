2a9a8921260e97f75bed26bc4f0724113864ebfd
Kevan Lynch <kevanlynch@gmail.com>
2016-11-23 21:07:36 -0400

    Added a BlackPerl

--- DontSink/Assets/Scripts/Objects/ShipObjects/PlayerShipObjects/DreadnoughtObject.cs
+++ DontSink/Assets/Scripts/Objects/ShipObjects/PlayerShipObjects/DreadnoughtObject.cs
@@ -7,7 +7,8 @@ public class DreadnoughtObject : PlayerShipObject
     static System.Collections.Generic.List<CrewObject> crew;
     static HullObject hull;
     static GameObject shipModel;
-    static string prefabPath = "Objects/Ships/Dreadnought";
+    //static string prefabPath = "Objects/Ships/Dreadnought";
+    static string prefabPath = "Objects/Ships/BlackPerl";
 
     public DreadnoughtObject() : base(10, 10, 10, hull,items, crew, shipModel,prefabPath) {
         shipModel = GameObject.FindGameObjectWithTag("Dreadnought");
