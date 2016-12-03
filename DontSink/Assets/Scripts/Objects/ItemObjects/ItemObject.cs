1e78a7a5ab9bbeef00b0b2b0addb5f2e1ec13546
Kevan Lynch <kevanlynch@gmail.com>
2016-12-03 19:35:27 -0400

    ItemObject parameters changes

--- DontSink/Assets/Scripts/Objects/ItemObjects/ItemObject.cs
+++ DontSink/Assets/Scripts/Objects/ItemObjects/ItemObject.cs
@@ -4,8 +4,14 @@ using System.Collections;
 public class ItemObject
 {
     //Fields
-    private Material item_texture;
+    private GameObject item_texture;
+    private Texture icon;
+    private string name;
+    private int val;
 
     //Accessors
-    public Material Item_Texture { get { return item_texture; } set { item_texture = value; } }
+    public GameObject Item_Texture { get { return item_texture; } set { item_texture = value; } }
+    public Texture Icon { get { return icon; } set { icon = value; } }
+    public string Name { get { return name; } set { name = value; } }
+    public int Value { get { return val; } set { val = value; } }
 }
