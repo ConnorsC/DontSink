981906350ddf107fe0c85ec28ef6b66a906c25ce
Kevan Lynch <kevanlynch@gmail.com>
2016-12-04 22:36:09 -0400

    Resolving conflicts

--- DontSink/Assets/Scripts/PlayerInformation.cs
+++ DontSink/Assets/Scripts/PlayerInformation.cs
@@ -3,18 +3,12 @@
 public class PlayerInformation {
 
     private static PlayerShipObject ship;
-<<<<<<< HEAD
     private int gold = 0;
-=======
-    private bool loadTurotial = true;
->>>>>>> refs/remotes/origin/Dev
+    private bool loadTutorial = true;
 
     public PlayerInformation() { }
 
     public PlayerShipObject Ship { get { return ship; } set { ship = value; } }
-<<<<<<< HEAD
     public int Gold { get { return gold; } set { gold = value; } }
-=======
-    public bool LoadTurotial { get { return loadTurotial; } set { loadTurotial = value; } }
->>>>>>> refs/remotes/origin/Dev
+    public bool LoadTurotial { get { return loadTutorial; } set { loadTutorial = value; } }
 }
