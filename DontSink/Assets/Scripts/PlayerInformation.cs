30c31a1f5f40a99b304721bf9c739d2f0eb33773
Kevan Lynch <kevanlynch@gmail.com>
2016-12-04 22:27:42 -0400

    Added a gold value to the player info
    Gold value which starts at 0 added to the player

--- DontSink/Assets/Scripts/PlayerInformation.cs
+++ DontSink/Assets/Scripts/PlayerInformation.cs
@@ -3,8 +3,10 @@
 public class PlayerInformation {
 
     private static PlayerShipObject ship;
+    private int gold = 0;
 
     public PlayerInformation() { }
 
     public PlayerShipObject Ship { get { return ship; } set { ship = value; } }
+    public int Gold { get { return gold; } set { gold = value; } }
 }
