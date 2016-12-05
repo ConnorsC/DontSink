fa238cfd4d6c966eab97527a7bcdd43d0768c0d2
Kevan Lynch <kevanlynch@gmail.com>
2016-12-04 21:28:27 -0400

    Added a distress scene and changed the defeat in the combat scene
    Distress scene you get a prompt from the enemy ship for help
    Help button no functionality at the moment but leave shows the distress
    ship sinking and shows a button to leave
    in combat scene when you defeat an enemy shows enemy sinking and shows
    button to leave

---
+++ DontSink/Assets/Scripts/CombatScreen/LeaveClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaveClick : MonoBehaviour
+{
+    public void ReturnToMap()
+    {
+        GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().LoadLevel("MapScreen");
+    }
+}
---
+++ DontSink/Assets/Scripts/DistressScreen/DistressClick.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistressClick : MonoBehaviour
+{
+    private GameManagerScript manager;
+    private GameObject leaveButton; 
+
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>();
+        leaveButton = GameObject.FindGameObjectWithTag("LeaveUI");
+        leaveButton.SetActive(false);
+    }
+
+    public void Help()
+    {
+
+    }
+
+    public void LeaveAlone()
+    {
+        GameObject distressShip = GameObject.FindGameObjectWithTag("DistressShip1");
+        ShipSink sink = distressShip.AddComponent<ShipSink>();
+        sink.sinkSpeed = 3;
+        RemoveUI();
+        CreateLeaveButton();
+    }
+    public void ReturnToMap()
+    {
+        manager.LoadLevel("MapScreen");
+    }
+    private void RemoveUI()
+    {
+        GameObject distressUI = GameObject.FindGameObjectWithTag("DistressUI");
+        distressUI.SetActive(false);
+    }
+    private void CreateLeaveButton()
+    {
+        leaveButton.SetActive(true);
+    }
+}
