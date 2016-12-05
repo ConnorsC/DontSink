ef3ccd8a302f5a325fa5e8591a176a4532112219
Kevan Lynch <kevanlynch@gmail.com>
2016-12-05 13:36:14 -0400

    Made lots of changes to add a boss island
    boss island always spawns DavyJones' ship may want to change that
    DavyJones' Ship is too big for the scene will likely have to zoom out in
    that scene.
    Clicking continue moves on to the next level
    Combat works just like the regular combat.

--- DontSink/Assets/Scripts/Objects/IslandObjects/EndIslandObject.cs
+++ DontSink/Assets/Scripts/Objects/IslandObjects/EndIslandObject.cs
@@ -3,5 +3,12 @@ using System.Collections;
 
 public class EndIslandObject : IslandObject
 {
+    private EnemyShipObject ship;
+    public EnemyShipObject Ship { get { return ship; } set { ship = value; } }
 
+    public EndIslandObject() { }
+    public EndIslandObject(EnemyShipObject ship)
+    {
+        this.ship = ship;
+    }
 }
