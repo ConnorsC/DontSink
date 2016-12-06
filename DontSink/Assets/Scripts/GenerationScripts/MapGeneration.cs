4447f8290377da1c6a3d12c0261539d61c5698ad
ConnorsC <connorschrisc@gmail.com>
2016-12-05 22:18:53 -0400

    Map 2 and 3 now work. changed a bunch of stuff

--- DontSink/Assets/Scripts/GenerationScripts/MapGeneration.cs
+++ DontSink/Assets/Scripts/GenerationScripts/MapGeneration.cs
@@ -22,6 +22,7 @@ public class MapGeneration
                 return null;
         }
     }
+
     private List<IslandObject> GenerateLevelOne()
     {
         List<int> islands = new List<int>();
@@ -31,9 +32,9 @@ public class MapGeneration
         int shop_island = rnd.Next(1, end_island); // Random number: 1<=shop_island<end_island
         //int shop_island = 1; // For the presentation set the shop island to island 2
         int distress_island = rnd.Next(1, end_island); // Random number: 1<=shop_island<end_island
-        while(shop_island == distress_island) // Ensure that we aren't trying to add the shop and distress islands to the same island number
+        while (shop_island == distress_island) // Ensure that we aren't trying to add the shop and distress islands to the same island number
             distress_island = rnd.Next(1, end_island);
-        for (int i = 0; i < end_island+1; i++)
+        for (int i = 0; i < end_island + 1; i++)
         {
             if (i == 0)
                 islands.Add(0); // Start Island
@@ -49,7 +50,6 @@ public class MapGeneration
         return island_gen.GenerateIslands(islands, level);
     }
 
-
     private List<IslandObject> GenerateLevelTwo()
     {
         List<int> islands = new List<int>();
@@ -62,8 +62,8 @@ public class MapGeneration
         int distress_island1;
         int distress_island2;
 
-        //9 is the ebd island 
-        var ints = new List<int>{1,2,3,4,5,6,7,8,10,11,12,13,14,15,16};
+        //9 is the end island 
+        var ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16 };
 
         int index = rnd.Next(ints.Count);
         shop_island1 = ints[index];
@@ -102,8 +102,57 @@ public class MapGeneration
         }
         return island_gen.GenerateIslands(islands, level);
     }
+
     private List<IslandObject> GenerateLevelThree()
     {
-        return new List<IslandObject>();
+        List<int> islands = new List<int>();
+        IslandGenerator island_gen = new IslandGenerator();
+        const int level = 3;
+        const int totalIslands = 16;
+        int shop_island1;
+        int shop_island2;
+        int shop_island3;
+        int distress_island1;
+        int distress_island2;
+
+        //9 is the end island 
+        var ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16 };
+
+        int index = rnd.Next(ints.Count);
+        shop_island1 = ints[index];
+        ints.RemoveAt(index);
+
+        index = rnd.Next(ints.Count);
+        shop_island2 = ints[index];
+        ints.RemoveAt(index);
+
+        index = rnd.Next(ints.Count);
+        shop_island3 = ints[index];
+        ints.RemoveAt(index);
+
+        index = rnd.Next(ints.Count);
+        distress_island1 = ints[index];
+        ints.RemoveAt(index);
+
+        index = rnd.Next(ints.Count);
+        distress_island2 = ints[index];
+        ints.RemoveAt(index);
+
+        for (int i = 0; i < totalIslands + 1; i++)
+        {
+            if (i == 0)
+                islands.Add(0); // Start Island
+            else if (i == shop_island1 || i == shop_island2 || i == shop_island3)
+                islands.Add(1); // Merchant Island
+
+            else if (i == distress_island1 || i == distress_island2)
+                islands.Add(3); // Distress Island
+
+            else if (i == 9)
+                islands.Add(4); // End Island
+            else
+                islands.Add(2);// Enemy Island
+        }
+        return island_gen.GenerateIslands(islands, level);
     }
 }
--- DontSink/Assets/Scripts/MapScreen/IslandMouseOverScript.cs
+++ DontSink/Assets/Scripts/MapScreen/IslandMouseOverScript.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 public class IslandMouseOverScript : MonoBehaviour
 {
     public int island;
+    private GameManagerScript manager;
+    private LineRenderer islandLine;
+    public Material lineMaterial;
     static string island1 = "Objects/Lines/OverlayIsland1";
     static string island2 = "Objects/Lines/OverlayIsland2";
     static string island3 = "Objects/Lines/OverlayIsland3";
@@ -17,44 +20,67 @@ public class IslandMouseOverScript : MonoBehaviour
     // Use this for initialization
 
     void Start () {
-	
-	}
+
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>();
+        islandLine = GetComponent<LineRenderer>();
+        islandLine.SetVertexCount(8);
+        islandLine.SetWidth(.2f, .2f);
+        islandLine.material = lineMaterial;
+    }
 	
 	void OnMouseOver ()
     {
-	    switch(island)
-        {
-            case 1:
-                overlay = Instantiate(Resources.Load(island1, typeof(GameObject))) as GameObject;
-                break;
-            case 2:
-                overlay = Instantiate(Resources.Load(island2, typeof(GameObject))) as GameObject;
-                break;
-            case 3:
-                overlay = Instantiate(Resources.Load(island3, typeof(GameObject))) as GameObject;
-                break;
-            case 4:
-                overlay = Instantiate(Resources.Load(island4, typeof(GameObject))) as GameObject;
-                break;
-            case 5:
-                overlay = Instantiate(Resources.Load(island5, typeof(GameObject))) as GameObject;
-                break;
-            case 6:
-                overlay = Instantiate(Resources.Load(island6, typeof(GameObject))) as GameObject;
-                break;
-            case 7:
-                overlay = Instantiate(Resources.Load(island7, typeof(GameObject))) as GameObject;
-                break;
-            case 8:
-                overlay = Instantiate(Resources.Load(island8, typeof(GameObject))) as GameObject;
-                break;
-            default:
-                break;
+
+        List<int> connectedIslands = FindIsland(island).ConnectedIsland;
+
+        int target1 = connectedIslands[0];
+        int target2 = connectedIslands[1];
+        int target3 = connectedIslands[2];
+        int target4 = connectedIslands[3];
+        print("my Island: " + island + " connections :\n " + connectedIslands[0] +"   "+ connectedIslands[1] + "   " + connectedIslands[2] + "   " + connectedIslands[3]);
+    
+        islandLine.enabled=true;
+
+        if (target1 != 0) {
+            islandLine.SetPosition(0, transform.position);
+            islandLine.SetPosition(1, FindIsland(target1).IslandModel.transform.position);
+            islandLine.SetPosition(2, transform.position);
         }
-	}
+        if (target2  != 0) {       
+            islandLine.SetPosition(3, FindIsland(target2).IslandModel.transform.position);
+            islandLine.SetPosition(4, transform.position);
+        }
+        if (target3 != 0)
+        {
+            islandLine.SetPosition(5, FindIsland(target3).IslandModel.transform.position);
+            islandLine.SetPosition(6, transform.position);
+        }
+        if (target4 != 0)
+        {
+            islandLine.SetPosition(7, FindIsland(target4).IslandModel.transform.position);
+        }
+    }
+
+    public IslandObject FindIsland(int islandToFind)
+    {
+        List<IslandObject> islands = manager.Islands;
+
+        for(int x =0; x<= islands.Count; x++)
+        {
+
+            if(islands[x].IslandNumber == islandToFind)
+            {
+                return islands[x];
+            }
+        }
+
+        print("couldent find island: " + islandToFind);
+        return null;
+    }
 
     void OnMouseExit()
     {
+        islandLine.enabled = false;
         GameObject[] overlays = GameObject.FindGameObjectsWithTag("IslandOverlayLine");
         foreach (GameObject line in overlays)
         {
